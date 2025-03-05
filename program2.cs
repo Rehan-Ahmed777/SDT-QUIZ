4a3921ba8be6efc778946c982da714574fc87c60
Rehan_Ahmed_ <127581895+Rehan-Ahmed777@users.noreply.github.com>
2025-03-05 11:52:30 +0500

    Add files via upload
    Quiz Question #2

---
+++ program2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = {
+            {9, 1, 8},
+            {2, 3, 7},
+            {6, 5, 4}
+        };
+
+            }
+        }
+    }
